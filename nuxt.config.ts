3cd0f57abc2902f80e9855e9245c09cef989dff9
pubuyun <wzh.zhang@qq.com>
2026-06-07 01:29:39 +0800

    fix studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,9 @@ export default defineNuxtConfig({
     css: ["./app/styles/main.css"],
     devtools: { enabled: true },
     compatibilityDate: "2024-04-03",
+    nitro: {
+        preset: "cloudflare-pages",
+    },
     content: {
         build: {
             markdown: {
