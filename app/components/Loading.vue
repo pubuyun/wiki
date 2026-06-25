aee6657dbb390a20e4c41ce15be58e8e4f3c3ba8
Ivy C. <ivychenofst@outlook.com>
2026-06-25 07:54:56 +0000

    Update file Loading.vue

--- app/components/Loading.vue
+++ app/components/Loading.vue
@@ -5,6 +5,9 @@
             class="fixed inset-0 z-9999 flex items-center justify-center bg-primary-deep backdrop-blur-sm"
         >
             <img :src="loadingImageUrl" alt="Loading" class="h-96" />
+            <p class="mt-4 text-white text-xl font-semibold tracking-wide">
+  Loading...
+</p>
             <span class="sr-only">Loading...</span>
         </div>
     </Transition>
