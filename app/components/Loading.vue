4c823642bcb2ae5253bf385e0a1804838470c9a7
Ivy C. <ivychenofst@outlook.com>
2026-06-25 08:04:49 +0000

    Fix loading text position

--- app/components/Loading.vue
+++ app/components/Loading.vue
@@ -4,10 +4,12 @@
             v-if="showLoading"
             class="fixed inset-0 z-9999 flex flex-col items-center justify-center bg-primary-deep backdrop-blur-sm"
         >
-            <img :src="loadingImageUrl" alt="Loading" class="h-96" />
-            <p class="mt-4 text-white text-xl font-semibold tracking-wide">
-  Loading...
-</p>
+         <div class="flex flex-col items-center">
+  <img :src="loadingImageUrl" alt="Loading" class="h-96" />
+  <p class="mt-4 text-white text-xl font-semibold tracking-wide">
+    Loading...
+  </p>
+</div>
             <span class="sr-only">Loading...</span>
         </div>
     </Transition>
