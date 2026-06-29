f1bc26d131c8f02902e667ada93248b0125f2fde
a01na <alinalin0106@outlook.com>
2026-06-29 18:12:29 +0800

    back to top文字一直可见

--- app/components/BackToTop.vue
+++ app/components/BackToTop.vue
@@ -57,7 +57,7 @@
             <img
                 src="https://static.igem.wiki/teams/6133/wiki/general/backtotop-new.avif"
                 alt="Back to top"
-                class="top-0 left-0 z-100 h-full w-full object-contain"
+                class="absolute inset-0 z-10 h-full w-full object-contain"
                 loading="lazy"
                 decoding="async"
             />
